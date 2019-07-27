9d2e2261bc0effb5b8fabe7ba0b450f1e353b1ed
Sergio Meneses <chicoboliviano16@gmail.com>
2019-07-27 18:39:44 -0400

    Final

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import TodoItem from './TodoItem'
+import PropTypes from 'prop-types'
 
-export default class Todos extends Component {
+
+class Todos extends Component {
+    
+    
     render() {
-        return (
-            <div>
-                <h1>todos</h1>
-            </div>
-        )
+ 
+        return this.props.todos.map((todo) => (
+            <TodoItem key={todo.id} 
+                todo = {todo} 
+                markComplete={this.props.markComplete}
+                delTodo = {this.props.delTodo}
+                />
+            )) 
     }
 }
+//proptypes
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired
+}
+export default Todos;
+
